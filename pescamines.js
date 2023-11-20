c81b78337346fd92390259174aaecdd7c0a213d0
angel_ivanov <angelspsv@gmail.com>
2023-11-20 19:59:27 +0100

    fent la taula dinàmica

--- pescamines.js
+++ pescamines.js
@@ -1,9 +1,31 @@
 function iniciarPartida(){
-
+    let num = parseInt(prompt("Entre el nombre de files del quadrat del pescamines:"));
+    if (num <= 10) {
+        num = 10;
+        crearTaulell(num);
+    } else if (num >= 30) {
+        num = 30;
+        crearTaulell(num);
+    } else {
+        crearTaulell(num);
+    }
 };
 
 
-
-function crearTaulell(){
-
+//función que crea una tabla NxN
+function crearTaulell(num){
+    //formación de la tabla de NxN dimensiones
+    let taulell = document.getElementById("taulell");
+    for(let i=0; i<num; i++){
+        let fila = taulell.insertRow();
+        
+        for(let j=0; j<num; j++){
+            let col = fila.insertCell(j);
+            col.innerHTML = "?";
+        
+            col.addEventListener("click", function() {
+                alert(`Has hecho clic en la celda (${i + 1}, ${j + 1})`);
+            });
+        }
+    }
 };
